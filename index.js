fa1cc18c66456a9421fae2a3616e0e651db59d3f
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-06 22:14:28 +0530

    added basic functionality of save feature

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
 console.log("Welcome to our todo app");
 
+let todos = [];
 
-let getTodosButton = document.getElementById('getTodos');
-let todoElement = document.getElementsByClassName('todo-details');
-let todoAction = document.getElementsByClassName('todo-action');
+let todoDataSection = document.getElementById("todo-data");
+let saveButton = document.getElementById("saveTodo");
+let todoInputBar = document.getElementById("todo-input-bar");
 
-//registration of event listener
-// getTodosButton.addEventListener("click",() => {
-//     console.log("Clicked");
-// })
+todoInputBar.addEventListener('keyup',function toggleSaveButton() {
+    const data = todoInputBar.value;
+    if(data.length === 0) {
+        if(saveButton.classList.contains('disabled')) return;
+        saveButton.classList.add('disabled');
+    }
+    else if(saveButton.classList.contains('disabled')){
+        saveButton.classList.remove('disabled');
+    }
+    
+})
 
-todoElement[1].addEventListener("click",() => {
-    console.log("Enetered");
+saveButton.addEventListener('click',function getTextAndAdd() {
+    const data = todoInputBar.value;
+    if(data.length === 0) return;
+    todos.push(data);
+    addTodo(data,todos.length);
+    todoInputBar.value = '';
 })
 
 
-todoAction[1].addEventListener("mouseover",handler);
+function addTodo(todoData, todoCount) {
+    let rowDiv     = document.createElement('div');
+    let todoItem   = document.createElement('div');
+    let todoNumber = document.createElement('div');
+    let todoDetail = document.createElement('div');
+    let todoStatus = document.createElement('div');
+    let todoAction = document.createElement('div');
+    let deleteBtn  = document.createElement('button');
+    let finishedBtn= document.createElement('button');
+    let hr         = document.createElement('hr');
 
 
+    //adding classes
+    rowDiv.classList.add("row");
+    todoItem.classList.add("todo-item" ,"d-flex" ,"flex-row" ,"justify-content-between" ,"align-items-center");
+    todoNumber.classList.add("todo-no");
+    todoDetail.classList.add("todo-details", "text-muted");
+    todoStatus.classList.add("todo-status", "text-muted");
+    todoAction.classList.add("todo-action", "d-flex" ,"justify-content-start", "gap-2");
+    deleteBtn.classList.add("btn", "btn-danger");
+    finishedBtn.classList.add("btn", "btn-success");
 
-function handler() {
-    console.log("hiii");
-}
+    
+    todoNumber.textContent = `${todoCount}.`;
+    todoDetail.textContent = todoData;
+    todoStatus.textContent = 'In progress';
+    deleteBtn.textContent = 'Delete';
+    finishedBtn.textContent = 'Finished';
 
 
-getTodosButton.onclick = () => {
-    console.log("hey from without event listener");
-}
+    todoAction.appendChild(deleteBtn);
+    todoAction.appendChild(finishedBtn);
 
-function clickBtn() {
-    console.log("click......");
+    todoItem.appendChild(todoNumber);
+    todoItem.appendChild(todoDetail);
+    todoItem.appendChild(todoStatus);
+    todoItem.appendChild(todoAction);
+
+    rowDiv.appendChild(todoItem);
+    rowDiv.appendChild(hr);
+
+    todoDataSection.appendChild(rowDiv);
 }
