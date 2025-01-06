8750df7bb1fa889f6c1a7949f9d63e0aa4591b04
Reema Dhanwani <reemadhanwani1234@gmail.com>
2025-01-06 20:30:28 +0530

    example of events

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Welcome to our todo app");
+
+
+let getTodosButton = document.getElementById('getTodos');
+let todoElement = document.getElementsByClassName('todo-details');
+let todoAction = document.getElementsByClassName('todo-action');
+
+//registration of event listener
+getTodosButton.addEventListener("click",() => {
+    console.log("Clicked");
+})
+
+todoElement[1].addEventListener("click",() => {
+    console.log("Enetered");
+})
+
+
+todoAction[1].addEventListener("mouseover",handler);
+
+
+
+function handler() {
+    console.log("hiii");
+}
